73b4e0c9510d4c30403247055523cec4e760ba42
Marcos Mendes <marcosdev.me@gmail.com>
2023-09-08 15:00:31 -0300

    docs: update readme (#1)
    * docs: update readme
    
    * fix: add props to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Main from '@/components/Main'
 
 export default function Home() {
-  return <Main />
+  return (
+    <Main
+      title={'Boilerplate Apps Router'}
+      description={'A custom boilerplate'}
+    />
+  )
 }
